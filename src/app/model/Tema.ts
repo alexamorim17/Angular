14678977eba9b26c1799b97cd0955d0fde2b0e59
Alexandre <fulanodetal@exemplo.br>
2022-03-03 11:12:22 -0300

    Adicionando função de listar tema

--- src/app/model/Tema.ts
+++ src/app/model/Tema.ts
@@ -2,9 +2,8 @@ import { Postagem } from "./Postagem"
 
 export class Tema{
 
-    public id:number
-    public nome:string
-    public descricao:string
-    public postagem:Postagem[]
+    public id: number;
+	public descricao: string;
+	public postagem: Postagem[];
 
 }
